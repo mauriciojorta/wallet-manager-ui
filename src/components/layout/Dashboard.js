83cb4b788f8efde38f2ea0472ffeb5ef14b47905
Mauricio Orta <mauriciojose.orta@babel.es>
2021-05-15 18:22:58 +0200

    Layout and customers, wallets and transfer pages components added.

---
+++ src/components/layout/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Customers from '../customer/Customers';
+
+const Dashboard = () => {
+  return (
+    <div className="container-fluid">
+        <Customers />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/wallet/Wallets.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import Spinner from '../layout/Spinner';
+
+import WalletManagerApi from '../../api/WalletManagerAPI';
+
+import { Link } from 'react-router-dom';
+
+
+export default class Wallets extends Component {
+    state = {
+        wallets: [],
+        email: ''
+    };
+
+    componentDidMount() {
+
+        const params = new URLSearchParams(this.props.location.search); 
+        const email = params.get('email');
+        WalletManagerApi.getWalletsOfCustomer(email)
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ wallets: data, email: email})
+            })
+            .catch(console.log)
+    }
+
+    render() {
+        if (this.state.wallets) {
+        return (
+            <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                {' '}
+                <i className="fas fa-wallet" /> Wallets of {this.state.email}
+              </h2>
+            </div>
+          </div>
+            {this.state.wallets.map(wallet => (
+                <div key={wallet.hash} className="card border-primary mb-3">
+                    <div className="card-header">{wallet.name}</div>
+                    <div className="card-body text-primary">
+                        <h5 className="card-title">Hash</h5>
+                        <p className="card-text">{wallet.hash}</p>
+                    </div>
+                    <div className="card-footer">
+                      <div className="row">
+                        <div className="col-md-6">
+                          Balance: {wallet.balance} €
+                        </div>
+                        <div className="col-md-6">
+                        <Link to={`/transfer/?email=${this.state.email}&wallet=${wallet.hash}`}>
+                          <button type="button" class="btn btn-primary">Make a transfer</button>
+                          </Link>
+                        </div>
+                      </div>
+                      </div>
+                </div>
+              ))}
+          </div>
+        );
+        } else {
+            return <Spinner />;
+        }
+    }
+}
